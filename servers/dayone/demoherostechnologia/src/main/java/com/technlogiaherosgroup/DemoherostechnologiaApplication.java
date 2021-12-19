a35a9ca73f201dba922a69f2b0ba871cf8ea6eb0
chourabi-dev <chourabitahers62@gmail.com>
2021-12-19 17:46:09 +0100

    latest

---
+++ servers/dayone/demoherostechnologia/src/main/java/com/technlogiaherosgroup/DemoherostechnologiaApplication.java
@@ -0,0 +1,13 @@
+package com.technlogiaherosgroup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoherostechnologiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoherostechnologiaApplication.class, args);
+	}
+
+}
